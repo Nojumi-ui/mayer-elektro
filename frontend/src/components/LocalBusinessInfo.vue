c82fcf286130c632410542af319999fd0222cfc8
Nojumi-ui <herat.spices@gmail.com>
2025-08-21 20:11:18 +0200

    fixed Accessibility Settings large text bug

--- frontend/src/components/LocalBusinessInfo.vue
+++ frontend/src/components/LocalBusinessInfo.vue
@@ -46,19 +46,19 @@
                 </div>
               </div>
               
-              <div class="flex items-start">
+              <!--div class="flex items-start">
                 <div class="text-[#0097b2] mt-1 mr-4">
                   <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                   </svg>
                 </div>
-                <!--div>
+                <div>
                   <h4 class="font-medium text-gray-800 dark:text-gray-200">{{ t('location.phone_label') }}</h4>
                   <p class="text-gray-600 dark:text-gray-400 mt-1">
                     <a href="tel:+4940123456789" class="hover:text-[#0097b2] transition">+49 (0) 40 123 456 789</a>
                   </p>
-                </div-->
-              </div>
+                </div>
+              </div-->
               
               <div class="flex items-start">
                 <div class="text-[#0097b2] mt-1 mr-4">
